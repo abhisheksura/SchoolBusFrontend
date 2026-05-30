02bb0cb3206910320ad5a1c3f88dc38d272151df
Abhishek Sura <abhisheksura90@gmail.com>
2026-05-30 12:17:30 +0530

    Assets directory restructuring

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+// src/assets/index.ts
+
+// 1. Core Framework & Tooling Logos (from assets/images/logos/)
+import reactLogo from "./images/logos/react.svg";
+import viteLogo from "./images/logos/vite.svg";
+
+// 2. Application Visual Graphics (from assets/images/heroes/)
+import loginHeroBg from "./images/heroes/hero.png";
+
+// 3. Clean Named Exports
+export {
+    reactLogo,
+    viteLogo,
+    loginHeroBg
+};
